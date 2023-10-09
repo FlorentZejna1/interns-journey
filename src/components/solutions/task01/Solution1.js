d52f5860d5e8b9c32a92b6c1ab2b7149d26a21cf
Maldrit <maldritm@gmail.com>
2023-10-09 14:32:16 +0200

    Created a Hello World Function

--- src/components/solutions/task01/Solution1.js
+++ src/components/solutions/task01/Solution1.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Hello from "./HelloWorld";
 
 function Solution1(props) {
   return (
     <div>
       <h1>Solution for assignment #{props.assignmentId}</h1>
+      <Hello />
     </div>
   );
 }
