55f6096d6d84d35b64cf2ddd4bd709a4a9f74c32
Maldrit <73648285+Maldrit@users.noreply.github.com>
2023-10-11 10:58:02 +0200

    Made a functional component that returns an image (#9)

---
+++ src/components/solutions/task05/Image.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Image (){
+    return (
+        <img src="https://assets.toptal.io/images?url=https%3A%2F%2Fbs-uploads.toptal.io%2Fblackfish-uploads%2Fcomponents%2Fblog_post_page%2Fcontent%2Fcover_image_file%2Fcover_image%2F1282565%2Fretina_500x200_cover-react-context-api-4929b3703a1a7082d99b53eb1bbfc31f.png"/>
+    )
+}
+
+export default Image
