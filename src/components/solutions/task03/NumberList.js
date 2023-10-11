ea2f1d793a1255732e387e5cf1c96ead2ebe8daa
Maldrit <maldritm@gmail.com>
2023-10-11 10:36:02 +0200

    Added the for loop that makes the list number 1 to 10

--- src/components/solutions/task03/NumberList.js
+++ src/components/solutions/task03/NumberList.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 
 function NumberList (){
-    const [num, setNum] = useState([1,2,3,4,5,6,7,8,9,10])
-    const list = num.map((num) => 
-        <li>{num}</li>
-    )
+    let num = [];
+    for(let i= 0; i < 10; i++){
+        num.push(<li>{i + 1}</li>)
+    } 
     return (
-        <ul>{list}</ul>
+        <ul>{num}</ul>
         )
 }
 
