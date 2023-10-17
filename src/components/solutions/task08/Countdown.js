85f89afcb0c17ba2bcf248f98a3c928c671a0e2e
Maldrit <maldritm@gmail.com>
2023-10-17 16:15:24 +0200

    Make a countdown timer

---
+++ src/components/solutions/task08/Countdown.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react"
+import "../style/solutions.css"
+
+
+function Countdown() {
+  const [timer, setTimer] = useState("");
+  const [start, setStart] = useState(false);
+  useEffect(() => {
+    if (start)
+      if (timer > 0) {
+        const interval = setInterval(() => {
+          setTimer(timer - 1);
+        }, 1000);
+
+        return () => clearInterval(interval);
+      }
+  });
+
+  const handleChange = (e) => {
+    setTimer(e.target.value.replace(/\D/, ""));
+  };
+
+  return (
+    <div className="countdown-container">
+      <h1 className="countdown-title">Countdown</h1>
+      {!start ? (
+        <input
+          className="countdown-input"
+          value={timer}
+          onChange={handleChange}
+          placeholder="Add a number"
+        />
+      ) : timer == 0 ? (
+        <h1 className="countdown-warning">Time is out!</h1>
+      ) : (
+        <p className="countdown-timer">{timer}</p>
+      )}
+      <button
+        className="countdown-button"
+        onClick={() => {
+          setStart(!start);
+        }}
+      >
+        start/stop{" "}
+      </button>
+    </div>
+  );
+}
+
+export default Countdown
--- src/components/solutions/task08/Solution8.js
+++ src/components/solutions/task08/Solution8.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Countdown from "./Countdown"
 
 function Solution8(props) {
   return (
     <div>
       <h1>Solution for assignment #{props.assignmentId}</h1>
+      <Countdown />
     </div>
   );
 }
