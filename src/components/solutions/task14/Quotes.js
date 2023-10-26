9ece7851ccc6b5d979239bd7f2295211b032a0e0
Maldrit <maldritm@gmail.com>
2023-10-26 16:42:17 +0200

    Make a random quotes app

---
+++ src/components/solutions/task14/Quotes.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+
+function Quotes() {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const quotes = [
+    {
+      quote:
+        "Carry out a random act of kindness, with no expectation of reward, safe in the knowledge that one day someone might do the same for you.",
+      author: "Princess Diana",
+    },
+    {
+      quote:
+        "I believe life is an intelligent thing: that things aren't random.",
+      author: "Steve Jobs",
+    },
+    {
+      quote:
+        "Even if I knew that tomorrow the world would go to pieces, I would still plant my apple tree.",
+      author: "Martin Luther",
+    },
+    {
+      quote:
+        "Ah, but a man's reach should exceed his grasp, Or what's a heaven for?",
+      author: "Robert Browning",
+    },
+    {
+      quote: "A person who never made a mistake never tried anything new.",
+      author: "Albert Einstein",
+    },
+    {
+      quote: "He will win who knows when to fight and when not to fight.",
+      author: "Sun Tzu",
+    },
+    {
+      quote: "Attitude is a little thing that makes a big difference.",
+      author: "Winston Churchill",
+    },
+    {
+      quote:
+        "I'm a dreamer. I have to dream and reach for the stars, and if I miss a star then I grab a handful of clouds.",
+      author: "Mike Tyson",
+    },
+    {
+      quote:
+        "Quit, dont quit? Noodles, don't noodles. You are too concerned about what was and what will be.",
+      author: "Master Oogway",
+    },
+    {
+      quote:
+        "Yesterday is history, tomorrow is a mystery, but today is a gift. That is why it is called the present.",
+      author: "Master Oogway",
+    },
+  ];
+
+  const randomGenerator = () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[randomIndex].quote;
+    const author = quotes[randomIndex].author;
+    setQuote(randomQuote);
+    setAuthor(author);
+  };
+  return (
+    <div className="quote-container">
+      {quote === "" ? (
+        <button className="quote-button" onClick={randomGenerator}>Generate a random quote</button>
+      ) : (
+        <>
+          <h1 className="quote-text">"{quote}"</h1>
+          <h1 className="quote-author">-{author}</h1>
+          <button className="quote-button" onClick={randomGenerator}>Generate a random quote</button>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Quotes;
--- src/components/solutions/task14/Solution14.js
+++ src/components/solutions/task14/Solution14.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Quotes from "./Quotes";
 
 function Solution14(props) {
   return (
     <div>
       <h1>Solution for assignment #{props.assignmentId}</h1>
+      <Quotes />
     </div>
   );
 }
