a3562a201c0ae8150f7b2a14ef6057cf1fc64d26
Maldrit <73648285+Maldrit@users.noreply.github.com>
2023-11-02 16:13:09 +0100

    Task14 | Make a random quotes app (#18)
    * Make a random quotes app
    
    * Move the Quotes array to another file

--- src/components/solutions/task14/Solution14.js
+++ src/components/solutions/task14/Solution14.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Quotes from "./Quotes";
+import { quotes } from "./QuotesList";
 
 function Solution14(props) {
   return (
     <div>
       <h1>Solution for assignment #{props.assignmentId}</h1>
+      <Quotes quotes={quotes} />
     </div>
   );
 }
