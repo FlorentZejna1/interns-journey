56c926a39b6337c3bb9310d32ed7cc9a35ca12e7
Maldrit <73648285+Maldrit@users.noreply.github.com>
2023-11-02 16:02:14 +0100

    Task11 | Make a basic calculator (#15)
    * Make a basic calculator
    
    * Made small changes

--- src/components/solutions/task11/Solution11.js
+++ src/components/solutions/task11/Solution11.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Calculator from "./Calculator";
 
 function Solution11(props) {
   return (
     <div>
       <h1>Solution for assignment #{props.assignmentId}</h1>
+      <Calculator />
     </div>
   );
 }
